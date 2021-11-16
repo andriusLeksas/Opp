2ae7830fd4358bd5b76d6f933d0685fa71d746b4
tomel1s <tomsin418@gmail.com>
2021-11-16 11:44:03 +0200

    OPP

---
+++ opp-projektas-All-merged/BoardGame/src/com/boardgame/Unit_Testing/PlayerIconTest.java
@@ -0,0 +1,12 @@
+package com.boardgame.Unit_Testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerIconTest {
+
+    @Test
+    void icon() {
+    }
+}
